73dcf07a1f5ddfcb55b3364d482db8c8170d0eed
Thomas Rusch <tomrr6@hotmail.com>
2022-12-07 14:13:12 -0600

    profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './profile.css';
 
 const Profile = () => {
     return (
@@ -10,7 +11,49 @@ const Profile = () => {
                 height: '100vh'
             }}
         >
-            <h1>Profile</h1>
+            
+
+
+<section class='main'>
+<h1>Settings</h1>
+<div class='profile' id='userIMG'></div>
+<input type="text" class='textBox' value="Thomas Rusch"></input>
+<h2>Display Settings</h2>
+<div class="displaySettings">
+	<div class="accentColorRow">
+	<div class="nochangeText">
+		<p>Accent Color</p>
+	</div>
+	<div class="accentColor">
+
+	</div>
+</div>
+<div class="categoryColumn">
+	<div class="category" id="redcat">
+		<p class="catName">
+			Work
+		</p>
+	</div>
+	<div class="category" id="bluecat">
+		<p class="catName">
+			School
+		</p>
+	</div>
+	<div class="category" id="greencat">
+		<p class="catName">
+			Other
+		</p>
+	</div>
+</div>
+</div>
+</section>
+
+
+
+
+
+
+
         </div>
     );
 };
