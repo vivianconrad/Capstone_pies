e737cd21a9224704fe5b9aeca52e222809996fe1
Restyled.io <commits@restyled.io>
2022-12-08 15:46:20 +0000

    Restyled by prettier

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,54 +1,56 @@
 import "../css/store.css";
 
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 /* It's a page that displays a list of hats that the user can buy */
 export default class Store extends Component {
   render() {
-                return (
-			<div className={"app_page"}>
-				<div className="top_header">
-					<h1 class="title">Shop</h1>{" "}
-				</div>
-				<h2>You have {localStorage.getItem("coins")} coins</h2>
-				<div class="container">
-					<div class="card">
-						<img
-							class="card--avatar"
-							src="../images/strawhat.png"
-                alt = "straw hat collectable"
-                width =
-                { "300" } />
-						<h1 class="card--title">Straw Hat</h1 >
-                    <h2>Price: 42 Coins<
-                        /h2>
-						<button class="card--click">Equip</button>
-                    </div>
-				</div><div class = "container">
-                    <div class = "card">< img
-                class = "card--avatar"
-                src = "../images/TopHat.png"
-                alt = "top hat collectable"
-                width =
-                { "300" } />
-						<h1 class="card--title">Top Hat</h1 >
-                    <h2>Price: 64 Coins<
-                        /h2>
-						<button class="card--click">Equip</button>
-                    </div>
-				</div><div class = "container">
-                    <div class = "card">< img
-                class = "card--avatar"
-                src = "../images/BowlerHat.png"
-                alt = "Bowler hat collectable"
-                width =
-                { "300" } />
-						<h1 class="card--title">Bowler Hat</h1 >
-                    <h2>Price: 610 Coins<
-                        /h2>
-						<button class="card--click">Equip</button>
-                    </div>
-				</div>< /div>
-		);
-	}
+    return (
+      <div className={"app_page"}>
+        <div className="top_header">
+          <h1 class="title">Shop</h1>{" "}
+        </div>
+        <h2>You have {localStorage.getItem("coins")} coins</h2>
+        <div class="container">
+          <div class="card">
+            <img
+              class="card--avatar"
+              src="../images/strawhat.png"
+              alt="straw hat collectable"
+              width={"300"}
+            />
+            <h1 class="card--title">Straw Hat</h1>
+            <h2>Price: 42 Coins</h2>
+            <button class="card--click">Equip</button>
+          </div>
+        </div>
+        <div class="container">
+          <div class="card">
+            <img
+              class="card--avatar"
+              src="../images/TopHat.png"
+              alt="top hat collectable"
+              width={"300"}
+            />
+            <h1 class="card--title">Top Hat</h1>
+            <h2>Price: 64 Coins</h2>
+            <button class="card--click">Equip</button>
+          </div>
+        </div>
+        <div class="container">
+          <div class="card">
+            <img
+              class="card--avatar"
+              src="../images/BowlerHat.png"
+              alt="Bowler hat collectable"
+              width={"300"}
+            />
+            <h1 class="card--title">Bowler Hat</h1>
+            <h2>Price: 610 Coins</h2>
+            <button class="card--click">Equip</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
